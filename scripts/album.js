0e8ce9ca0c10603a7f7bccc7644cc6ec3002cfed
Melvin Yuen <melvinyuen@gmail.com>
2017-04-21 17:18:40 -0700

    removed code for checkpoint-11-assignment

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@ var albumMarconi = {
     ]
 };
 
+// Melvin Custom Example Album
+var albumMelvin = {
+    title: 'The Repeat',
+    artist: 'Melvin Yuen',
+    label: 'Red Rhino',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/19.png',
+    songs: [
+        { title: 'Did you Understand js?', duration: '1:11' },
+        { title: 'Starting to Come Back', duration: '2:22' },
+        { title: 'Pay Attention', duration: '3:33'},
+        { title: 'Can you see me now?', duration: '4:44' },
+        { title: 'Wrong phone syntax', duration: '5:55'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -43,28 +59,28 @@ var createSongRow = function(songNumber, songName, songLength) {
 };
 
 var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+     // #1
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-    // #2
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
+     // #2
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
 
-    // #3
-    albumSongList.innerHTML = '';
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
 
-    // #4
-    for (var i = 0; i < album.songs.length; i++) {
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    }
-};
-
-window.onload = function() {
-    setCurrentAlbum(albumPicasso);
-};
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+ };
