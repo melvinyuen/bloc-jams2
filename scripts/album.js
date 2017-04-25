bda30d5e4f5b2527ae82f2b26f3f74917f8be170
Melvin Yuen <melvinyuen@gmail.com>
2017-04-25 00:01:23 -0700

    added event listeners to the album view for play button when mouse over the song number

--- scripts/album.js
+++ scripts/album.js
@@ -30,41 +30,77 @@ var albumMarconi = {
     ]
 };
 
+// Melvin Custom Example Album
+var albumMelvin = {
+    title: 'The Repeat',
+    artist: 'Melvin Yuen',
+    label: 'Red Rhino',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/19.png',
+    songs: [
+        { title: 'Did you Understand js?', duration: '1:11' },
+        { title: 'Starting to Come Back', duration: '2:22' },
+        { title: 'Pay Attention', duration: '3:33'},
+        { title: 'Can you see me now?', duration: '4:44' },
+        { title: 'Wrong phone syntax', duration: '5:55'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
-     + '  <td class="song-item-title">' + songName + '</td>'
-     + '  <td class="song-item-duration">' + songLength + '</td>'
-     + '</tr>'
+       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+       + '  <td class="song-item-title">' + songName + '</td>'
+       + '  <td class="song-item-duration">' + songLength + '</td>'
+       + '</tr>'
      ;
 
     return template;
 };
 
 var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+     // #1
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-    // #2
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
+     // #2
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
 
-    // #3
-    albumSongList.innerHTML = '';
+     // #3
+     albumSongList.innerHTML = '';
 
-    // #4
-    for (var i = 0; i < album.songs.length; i++) {
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    }
-};
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
-    setCurrentAlbum(albumPicasso);
+   setCurrentAlbum(albumPicasso);
+
+   songListContainer.addEventListener('mouseover', function(event) {
+       // #1
+       if(event.target.parentElement.className === 'album-view-song-item') {
+         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+       }
+   });
+
+   for (var i = 0; i < songRows.length; i++) {
+       songRows[i].addEventListener('mouseleave', function(event) {
+           // Selects first child element, which is the song-item-number element
+           this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+       });
+   }
 };
