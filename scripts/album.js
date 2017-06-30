2b0ae98272c62124c485f3073cd98d61955a9ba3
Melvin Yuen <melvinyuen@gmail.com>
2017-06-30 14:53:22 -0700

    unfinished checkpoint-21, volume thumb not perfect

--- scripts/album.js
+++ scripts/album.js
@@ -12,6 +12,12 @@ var setSong = function(songNumber) {
 	setVolume(currentVolume);
 };
 
+var seek = function(time) {
+	if (currentSoundFile) {
+		currentSoundFile.setTime(time);
+	}
+}
+
 var setVolume = function(volume) {
 	if (currentSoundFile) {
 		currentSoundFile.setVolume(volume);
@@ -44,16 +50,26 @@ var createSongRow = function(songNumber, songName, songLength) {
     }
     if (currentlyPlayingSongNumber !== songNumber) {
       // Switch from Play -> Pause button to indicate new song is playing.
-      $(this).html(pauseButtonTemplate);
+
       setSong(songNumber);
 			currentSoundFile.play()
-      updatePlayerBarSong();
+			updateSeekBarWhileSongPlays();//; MY-2017-Jun-21 do I need this semicolon?
+			currentSongFromAlbum = currentAlbum.songs[songNumber -1];
+
+			var $volumeFill = $('.voume .fill');
+			var $volumeThumb = $('.volume .thumb');
+			$volumeFill.width(currentVolume + '%');
+			$volumeThumb.css({left: currentVolume + '%'});
+
+			$(this).html(pauseButtonTemplate);
+			updatePlayerBarSong();
     } else if (currentlyPlayingSongNumber === songNumber) {
       // Switch from Pause -> Play button to pause currently playing song.
       if (currentSoundFile.isPaused()) {
 				$(this).html(pauseButtonTemplate);
 				$('.main-controls .play-pause').html(playerBarPauseButton);
 				currentSoundFile.play();
+				updateSeekBarWhileSongPlays(); //; MY-2017-Jun-21 do I need to call updateSeekBarWhileSongPlays here as well?  MY: Double check
 			} else {
 				$(this).html(playButtonTemplate);
 				$('.main-controls .play-pause').html(playerBarPlayButton);
@@ -111,6 +127,66 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var updateSeekBarWhileSongPlays = function() {
+	if (currentSoundFile) {
+		currentSoundFile.bind('timeupdate', function(event) {
+			var seekBarFillRatio = this.getTime() / this.getDuration();
+			var $seekBar = $('.seek-control .seekBar');
+
+			updateSeekPercentage($seekBar, seekBarFillRatio);
+		});
+	}
+};
+
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+    var offsetXPercent = seekBarFillRatio * 100;
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({left: percentageString});
+ };
+var setupSeekBars = function() {
+  var $seekBars = $('.player-bar .seek-bar');
+
+  $seekBars.click(function(event) {
+    var offsetX = event.pageX - $(this).offset().left;
+    var barWidth = $(this).width();
+    var seekBarFillRatio = offsetX / barWidth;
+
+    if ($(this).parent().attr('class') == 'seek-control') {
+      seek(seekBarFillRatio * currentSoundFile.getDuration());
+    } else {
+      setVolume(seekBarFillRatio * 100);
+    }
+    updateSeekPercentage($(this), seekBarFillRatio);
+  });
+
+  $seekBars.find('.thumb').mousedown(function(event) {
+    var $seekBar = $(this).parent();
+
+    $(document).bind('mousemove.thumb', function(event) {
+      var offsetX = event.pageX - $seekBar.offset().left;
+      var barWidth = $seekBar.width();
+      var seekBarFillRatio = offsetX / barWidth;
+
+      if ($seekBar.parent().attr('class') == 'seek-control') {
+        seek(seekBarFillRatio * currentSoundFile.getDuration());
+      } else {
+        setVolume(seekBarFillRatio);
+      }
+
+      updateSeekPercentage($seekBar, seekBarFillRatio);
+    });
+
+    $(document).bind('mouseup.thumb', function() {
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+  });
+};
+
 var trackIndex = function(album, song) {
   return album.songs.indexOf(song);
 };
@@ -156,6 +232,7 @@ var nextSong = function() {
   // Set a new current song
   setSong(currentSongIndex + 1);
 	currentSoundFile.play();
+	updateSeekBarWhileSongPlays();//; MY-2017-Jun-21 do I need this semicolon?
   // Update the Player Bar Information
   updatePlayerBarSong();
 
@@ -181,6 +258,7 @@ var previousSong = function() {
   // Set a new current song
   setSong(currentSongIndex + 1);
 	currentSoundFile.play();
+	updateSeekBarWhileSongPlays();//; MY-2017-Jun-21 do I need this semicolon?
 
   // Update the Player Bar Information
   updatePlayerBarSong();
@@ -210,6 +288,7 @@ var togglePlayFromPlayerBar = function() {
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
+	setupSeekBars();
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
 	$playPauseButton.click(togglePlayFromPlayerBar);
